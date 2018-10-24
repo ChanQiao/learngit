385f973c83361e11c9cdb4909f8692aa5a611f55
chanqiao <993655985@qq.com>
2018-10-24 22:58:13 +0800

    添加学生信息

--- class/class.py
+++ class/class.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 
-print("hello world")
+class Student(object):
 
+    def __init__(self, name, source):
+        self.__name = name
+        self.__source = source
+
+    def show_info(self):
+        print("name : %s  source : %0.1f" % (self.__name, self.__source))
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+if __name__ == "__main__":
+    bob = Student("Bob", 89.6)
+    bob.show_info()
